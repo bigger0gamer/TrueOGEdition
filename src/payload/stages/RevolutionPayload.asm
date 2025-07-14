317f7e9a5791ac4f35abd337cc586e5000972396
Yuri <ragingwingboner@gmail.com>
2025-07-14 18:35:40 -0400

    Finalized New Revo (and oops never added the new file)

--- src/payload/stages/RevolutionPayload.asm
+++ src/payload/stages/RevolutionPayload.asm
@@ -21,7 +21,17 @@ RevoSpinCooldown:
 
 ForceSpinCounterReset:
   lui at,hi(NoHazCustomVar)
-  sw r0,lo(NoHazCustomVar)(at)
+  lw at,lo(HazardsVar)(at)
+  sw r0,lo(NoHazCustomVar)(at) :: .resetdelay
+  bne at,r0,@@Skip
+  li at,0x80073320
+  addi s0,r0,0x60
+  sh s0,0(at)
+  addi at,at,0x10
+  addi s0,r0,0xFF9F
+  sh s0,0(at)
+  
+  @@Skip:
   j ForceSpinCounterResetReturn
   sw r0,0x0D48(s1)  ; original instruction
   
@@ -76,4 +86,4 @@ RevoFauxCeiling:
   @@ForceSpin:
   addi at,r0,30*3
   j RevoSpinReturn
-  sw r0,0x0000(v0)
+  sw at,0x0000(v0)
---
+++ src/st0X/RevoSt2.asm
@@ -0,0 +1,29 @@
+.psx
+
+  .org 0x800D4CB0 :: RevolutionNoHazardsReturn:
+; 800D49EC RNG call, 1 in 512 chance of rotation every frame
+; 800D4CA8 if no spin
+; 800D49F4 if spin
+; bne v0,r0,0x800D4CA8  orig instruction
+
+; Spinny Spin logic
+.org 0x800D49F4 :: RevoSpinReturn:
+.org 0x800D4CA8 :: RevoNoSpinReturn:
+.org 0x800D49EC
+  j RevoFauxCeiling
+  lui at,hi(HazardsVar)
+
+; Spin cooldown adjuster
+.org 0x800D4C9C :: RevoSpinCooldownReturn:
+.org 0x800D4C90
+  lui v0,hi(HazardsVar)
+  j RevoSpinCooldown
+  lw v0,lo(HazardsVar)(v0)
+
+; So that it's always 0 at the start of round
+.org 0x800D45B4 :: ForceSpinCounterResetReturn:
+.org 0x800D45AC
+  j ForceSpinCounterReset
+
+; 800D4B14 -> Freeze players
+; 800D4C50 -> Unfreeze players
