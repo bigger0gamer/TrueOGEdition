e64921f27677e09ee99fc1632adfaee7bde41684
Yuri Bacon <ragingwingboner@gmail.com>
2025-07-19 18:47:09 -0400

    extrach.sh no longer looks for quickbms from local dir I figured out I can just copy it to /usr/bin instead lol

--- build env/extract.sh
+++ build env/extract.sh
@@ -3,6 +3,6 @@ mkdir cleanROM
 mv "Digimon Rumble Arena (US).cue" cleanROM
 mv "Digimon Rumble Arena (US).bin" cleanROM
 mkdir "Digimon Rumble Arena (US)/vfs"
-./quickbms digimon_vfs2.bms "Digimon Rumble Arena (US)/A.VFS" "Digimon Rumble Arena (US)/vfs"
+quickbms digimon_vfs2.bms "Digimon Rumble Arena (US)/A.VFS" "Digimon Rumble Arena (US)/vfs"
 mkdir "Digimon Rumble Arena (US)/inject"
 mkdir "Digimon Rumble Arena (US)/inject/bin"
